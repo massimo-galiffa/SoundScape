873252b6b2a9bb82b5f8382882eab2e4306df7f0
Massimo <96292441+massimo-galiffa@users.noreply.github.com>
2024-01-16 08:35:42 +0100

    [FIX] Home Page RecentlyPlayed has now image and styling

--- View/RecentlyPlayed.cs
+++ View/RecentlyPlayed.cs
@@ -7,6 +7,7 @@ namespace SoundScape.View
         public string Artist { get; set; }
         public string Title { get; set; }
         public string Duration { get; set; }
+        public string Image { get; set; } 
     }
 
     public class RecentlyPlayed
@@ -16,14 +17,14 @@ namespace SoundScape.View
         public RecentlyPlayed()
         {
             Songs = new ObservableCollection<Song>();
-            Songs.Add(new Song { Title = "STARGAZING", Artist = "Travis Scott", Duration = "3:21" });
-            Songs.Add(new Song { Title = "Gatti", Artist = "Travis Scott", Duration = "2:45" });
-            Songs.Add(new Song { Title = "FE!N", Artist = "Travis Scott", Duration = "4:00" });
-            Songs.Add(new Song { Title = "Nightcrawler", Artist = "Travis Scott", Duration = "4:04" });
-            Songs.Add(new Song { Title = "ASTROWORLD", Artist = "Travis Scott", Duration = "4:04" });
-            Songs.Add(new Song { Title = "Jackboys", Artist = "Travis Scott", Duration = "4:04" });
-            Songs.Add(new Song { Title = "UTOPIA", Artist = "Travis Scott", Duration = "4:04" });
-            Songs.Add(new Song { Title = "RODEO", Artist = "Travis Scott", Duration = "4:04" });
+            Songs.Add(new Song { Image = "astroworld.jpg", Title = "STARGAZING", Artist = "Travis Scott", Duration = "3:21" });
+            Songs.Add(new Song { Image = "jackboys.jpg", Title = "Gatti", Artist = "Travis Scott", Duration = "2:45" });
+            Songs.Add(new Song { Image = "utopia.png", Title = "FE!N", Artist = "Travis Scott", Duration = "4:00" });
+            Songs.Add(new Song { Image = "rodeo.png", Title = "Nightcrawler", Artist = "Travis Scott", Duration = "4:04" });
+            Songs.Add(new Song { Image = "astroworld.jpg", Title = "ASTROWORLD", Artist = "Travis Scott", Duration = "4:04" });
+            Songs.Add(new Song { Image = "jackboys.jpg", Title = "Jackboys", Artist = "Travis Scott", Duration = "4:04" });
+            Songs.Add(new Song { Image = "utopia.png", Title = "UTOPIA", Artist = "Travis Scott", Duration = "4:04" });
+            Songs.Add(new Song { Image = "rodeo.png", Title = "RODEO", Artist = "Travis Scott", Duration = "4:04" });
         }
     }
 }
