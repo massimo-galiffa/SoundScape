a657578f3234bf97de79e57aae0ce933a71c2be2
Massimo <96292441+massimo-galiffa@users.noreply.github.com>
2024-01-16 10:55:05 +0100

    [added] Logic for Login and Registration Button

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace SoundScape
+﻿using SoundScape.View;
+
+namespace SoundScape
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute("register", typeof(RegisterPage));
         }
     }
 }
--- View/SignOut.xaml.cs
+++ View/SignOut.xaml.cs
@@ -6,4 +6,14 @@ public partial class SignOut : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void OnGoToHomeClicked(object sender, EventArgs e)
+    {
+        // Navigate to the home page
+        await Shell.Current.GoToAsync("//HomePage");
+    }
+    private async void OnGoToRegisterClicked(object sender, EventArgs e)
+    {
+        // Navigate to the Register page
+        await Shell.Current.GoToAsync("register");
+    }
 }
